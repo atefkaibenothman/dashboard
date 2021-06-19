a31f6d2ed62bc97d00f5e33eb7478ea5e71dd6d0
atefkaibenothman <atefkaibenothman>
2021-06-18 23:18:46 -0700

    removed duplicated directory

--- web/dashboard_backend/base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class ActorAdmin(admin.ModelAdmin):
-    list_display = ("actor_id", "first_name", "last_name", "last_update")
-
-admin.site.register(Actor, ActorAdmin)
-admin.site.register(Film)
-admin.site.register(Category)
